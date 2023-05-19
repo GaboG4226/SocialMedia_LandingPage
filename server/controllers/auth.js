7cf3a2e879070b493da66d52c1da5300e9b3ef61
GaboG4226 <gabogarcia4226@outlook.com>
2023-05-19 15:17:42 -0600

    Added comments and ;

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -42,12 +42,16 @@ export const login = async(req, res) => {
     try {
        const  { email, password } =  req.body;
        const user  = await User.findOne({ email:email })
-       if(!user) return res.status(400).json({ msg: "User does not exist"}) // if user is not in database send error message
+       if(!user) return res.status(400).json({ msg: "User does not exist"}); // if user is not in database send error message
 
        const isMatch = await bcrypt.compare(password, user.password);
-       if(!isMatch) return res.status(400).json({ msg: "Invalid credentials"}) 
+       if(!isMatch) return res.status(400).json({ msg: "Invalid credentials"});
+
+       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+       delete user.password; // to avoid password to be sent to the frontend, just to keep it safe
+       res.status(200).json({ token, user });
 
     } catch (err) {
-        res.status(500).json({ error: err.message })
+        res.status(500).json({ error: err.message });
     }
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -12,9 +12,9 @@ export const verifyToken = async (req, res, next) => {
             token = token.slice(7, token.length).trimLeft();
         }
 
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user= verified;
-        next();
+        const verified = jwt.verify(token, process.env.JWT_SECRET); // if not verified it thows an error
+        req.user = verified; // if verified it returns the payloas
+        next(); // go to the next function that was passed as an argument
 
     } catch (err) {
         res.status(500).json({ error: err.message })
